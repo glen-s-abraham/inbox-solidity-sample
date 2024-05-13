a98be04515b627af0c0896e444ff38f1320a1e35
glen-s-abraham <glenprojects666@gmail.com>
2024-05-13 13:03:57 +0530

    deployment tests

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,45 +1,35 @@
 const assert = require('assert');
 const ganache = require('ganache');
-const { before } = require('mocha');
 const { Web3 } = require('web3');
-
+const { interface, bytecode } = require('../compile');
 const web3 = new Web3(ganache.provider());
 
-// class Car {
-//     park() {
-//         return 'stopped'
-//     }
-
-//     drive() {
-//         return 'vroom'
-//     }
-// }
-
-// describe('Car', () => {
-//     let car;
-//     beforeEach(() => {
-//         car = new Car();
-//     })
-//     it('can park', () => {
-
-//         assert.equal(car.park(), 'stopped');
-//     })
-
-//     it('can drive', () => {
-
-//         assert.equal(car.drive(), 'vroom');
-//     })
-
-// })
 
 let accounts;
+let inbox;
+const INITIAL_MESSAGE = 'Hi there!'
 
-beforeEach(async() => {
-    accounts = await  web3.eth.getAccounts()
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts()
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
+        .send({ from: accounts[0], gas: '1000000' })
 })
 
-describe('Inbox',()=>{
-    it('deploys a contract',()=>{
-        console.log(accounts)
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        // Check if the inbox cotnract instance has been created.
+        assert.ok(inbox.options.address);
+    })
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_MESSAGE)
+    })
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('Bye there!').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Bye there!')
     })
 })
